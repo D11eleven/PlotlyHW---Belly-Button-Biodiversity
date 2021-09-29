f35c4df05a62a26c8eafc9a9ebc87ba774359c3a
Darrell S Horich <d11eleven@gmail.com>
2021-09-29 13:39:58 -0500

    incremental save - start app.js

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # PlotlyHW---Belly-Button-Biodiversity
 
 
-#  Insert Starter Code and Data 
+#  Incremental Save  --    Start app.js 
 
 ![hw02](https://user-images.githubusercontent.com/82190357/134819862-2317d4ff-444a-4485-9392-fb3309d0c799.png)
---
+++ static/js/app.js
@@ -0,0 +1,62 @@
+
+
+
+// USE D3 TO READ IN 'SAMPLES.JSON'
+
+
+
+
+//Horizontal Bar   from 2-09
+
+// let sortedByGreekSearch = data.sort((a, b) => b.greekSearchResults - a.greekSearchResults);
+
+// // Slice the first 10 objects for plotting
+// slicedData = sortedByGreekSearch.slice(0, 10);
+
+// // Reverse the array to accommodate Plotly's defaults
+// reversedData = slicedData.reverse();
+
+// // Trace1 for the Greek Data
+// let trace1 = {
+//   x: reversedData.map(object => object.greekSearchResults),
+//   y: reversedData.map(object => object.greekName),
+//   text: reversedData.map(object => object.greekName),
+//   name: "Greek",
+//   type: "bar",
+//   orientation: "h"   <<<<<<<<<<<<<<<<<<<<<<<<<<<<    HORIZONTAL 
+// };
+
+// // Data array
+// // `data` has already been defined, so we must choose a new name here:
+// let traceData = [trace1];
+
+// // Apply a title to the layout
+// let layout = {
+//   title: "Greek gods search results",
+//   margin: {
+//     l: 100,
+//     r: 100,
+//     t: 100,
+//     b: 100
+//   }
+// };
+
+// // Render the plot to the div tag with id "plot"
+// // Note that we use `traceData` here, not `data`
+// Plotly.newPlot("plot", traceData, layout);
+
+
+
+// BUBBLE CHART
+
+
+// DISPLAY SAMPLE METADATA
+
+
+// DISPLAY KEY-VALUE PAIR FROM METADATA          HTML CARD   DEMOGRAPHIC INFO 
+
+
+// UPDATE ALL PLOTS  NEW SAMPLE SELECTED  
+
+
+
