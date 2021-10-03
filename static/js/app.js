ae634ce146f5334fc0c2fc02719a2d11f2947a48
Darrell S Horich <d11eleven@gmail.com>
2021-10-02 22:50:42 -0500

    setting up chart code and variables

--- static/js/app.js
+++ static/js/app.js
@@ -13,8 +13,99 @@ d3.json("data/samples.json").then(function(data) {
 // console.log(data); 
 
 
+// var sample_names
+// var sample_values
+// var otu_ids
+// var otu_labels
+//
+
+
+// var topTenValues
+// var
+
+
+
+
+
+
+///  init  dropdown   3-10
+
+// Create an array of each country's numbers
+// var us = Object.values(data.us);
+// var uk = Object.values(data.uk);
+// var canada = Object.values(data.canada);
+
+// // Create an array of music provider labels
+// var labels = Object.keys(data.us);
+
+// // Display the default plot
+// function init() {
+//   var data = [{
+//     values: us,
+//     labels: labels,
+//     type: "pie"
+//   }];
+
+//   var layout = {
+//     height: 600,
+//     width: 800
+//   };
+
+//   Plotly.newPlot("pie", data, layout);
+// }
+
+// // On change to the DOM, call getData()
+// d3.selectAll("#selDataset").on("change", getData);
+
+// // Function called by DOM changes
+// function getData() {
+//   var dropdownMenu = d3.select("#selDataset");
+//   // Assign the value of the dropdown menu option to a variable
+//   var dataset = dropdownMenu.property("value");
+//   // Initialize an empty array for the country's data
+//   var data = [];
+
+//   if (dataset == 'us') {
+//       data = us;
+//   }
+//   else if (dataset == 'uk') {
+//       data = uk;
+//   }
+//   else if (dataset == 'canada') {
+//       data = canada;
+//   }
+//   // Call function to update the chart
+//   updatePlotly(data);
+// }
+
+// // Update the restyled plot's values
+// function updatePlotly(newdata) {
+//   Plotly.restyle("pie", "values", [newdata]);
+// }
+
+// init();
+
+
+
+
+
+
 
 //Horizontal Bar   from 2-09
+
+// var data = [{
+//     type: 'bar',
+//     x: [20, 14, 23],
+//     y: ['giraffes', 'orangutans', 'monkeys'],
+//     orientation: 'h'
+//   }];
+  
+//   Plotly.newPlot('myDiv', data);
+
+
+
+
+//from 2-9
 
 // let sortedByGreekSearch = data.sort((a, b) => b.greekSearchResults - a.greekSearchResults);
 
@@ -31,7 +122,7 @@ d3.json("data/samples.json").then(function(data) {
 //   text: reversedData.map(object => object.greekName),
 //   name: "Greek",
 //   type: "bar",
-//   orientation: "h"   <<<<<<<<<<<<<<<<<<<<<<<<<<<<    HORIZONTAL 
+//   orientation: "h"
 // };
 
 // // Data array
@@ -55,13 +146,69 @@ d3.json("data/samples.json").then(function(data) {
 
 
 
+
+
+
 // BUBBLE CHART
+
+
+// var trace1 = {
+//     x: [1, 2, 3, 4],
+//     y: [10, 11, 12, 13],
+//     mode: 'markers',
+//     marker: {
+//       size: [40, 60, 80, 100]
+//     }
+//   };
+  
+//   var data = [trace1];
+  
+//   var layout = {
+//     title: 'Marker Size',
+//     showlegend: false,
+//     height: 600,
+//     width: 600
+//   };
+  
+//   Plotly.newPlot('myDiv', data, layout);
+
+
+
+
+// PIE CHART
+
+
+// var data = [{
+//     values: [19, 26, 55],
+//     labels: ['Residential', 'Non-Residential', 'Utility'],
+//     type: 'pie'
+//   }];
+  
+//   var layout = {
+//     height: 400,
+//     width: 500
+//   };
+  
+//   Plotly.newPlot('myDiv', data, layout);
+
+
+
 
 
 // DISPLAY SAMPLE METADATA
 
+// var metadata
+
+
+
+
+
+
+
 
 // DISPLAY KEY-VALUE PAIR FROM METADATA          HTML CARD   DEMOGRAPHIC INFO 
+
+
 
 
 // UPDATE ALL PLOTS  NEW SAMPLE SELECTED  
